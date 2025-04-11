ea896e21712274eb5b37190c7611304a35eb88f1
GHOST <3089308393@qq.com>
2025-04-11 23:45:16 +0800

    use env-enc

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@chainlink/env-enc").config();
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_URL, // Alchemy, Infura, QuickNode
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
