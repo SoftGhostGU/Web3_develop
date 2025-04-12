b14587fe6c0472993e454798832c1835657350cc
GHOST <3089308393@qq.com>
2025-04-12 15:11:01 +0800

    use .env.enc

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -3,6 +3,13 @@ require("@chainlink/env-enc").config();
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+// 加入系统代理，以确保能快速连接上sepolia测试网
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
+setGlobalDispatcher(proxyAgent);
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -11,7 +18,13 @@ module.exports = {
   networks: {
     sepolia: {
       url: SEPOLIA_URL, // Alchemy, Infura, QuickNode
-      accounts: [PRIVATE_KEY]
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111, // Sepolia chainId (searched on chainlist.org)
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
     }
   }
 };
