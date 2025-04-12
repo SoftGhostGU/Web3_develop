d431f1bccc6845411cff3d13b025e5f980663d2c
GHOST <3089308393@qq.com>
2025-04-12 20:15:47 +0800

    finish deployFundMe

--- hardhat/scripts/deployFundMe.js
+++ hardhat/scripts/deployFundMe.js
@@ -1,5 +1,11 @@
 // import ethers.js
 // create main function
+    // init 2 accounts
+    // fund contract with first account
+    // check balance of contract
+    // fund contract with second account
+    // check balance of contract
+    // check mapping fundersToAmount
 // execute main function
 
 const { ethers } = require("hardhat");
@@ -24,6 +30,31 @@ async function main() {
     } else {
         console.log("Skipping contract verification...");
     }
+
+    // init 2 accounts
+    const [firstAccount, secondAccount] = await ethers.getSigners();
+
+    // fund contract with first account
+    const fundTx = await fundMe.fund({value: ethers.parseEther("0.5")})
+    await fundTx.wait();
+
+    // check balance of contract
+    const balanceOfContract = await ethers.provider.getBalance(fundMe.target);
+    console.log("Balance of contract:", balanceOfContract);
+
+    // fund contract with second account
+    const fundTxWithSecondAccount = await fundMe.connect(secondAccount).fund({value: ethers.parseEther("0.5")})
+    await fundTxWithSecondAccount.wait();
+
+    // check balance of contract
+    const balanceOfContractAfterSecondFund = await ethers.provider.getBalance(fundMe.target);
+    console.log("Balance of contract:", balanceOfContractAfterSecondFund);
+
+    // check mapping fundersToAmount
+    const firstAccountBalanceInFundMe = await fundMe.fundersToAmount(firstAccount.address);
+    const secondAccountBalanceInFundMe = await fundMe.fundersToAmount(secondAccount.address);
+    console.log("Balance of first account " + firstAccount.address + " in FundMe:", firstAccountBalanceInFundMe);
+    console.log("Balance of second account " + secondAccount.address + " in FundMe:", secondAccountBalanceInFundMe);
 }
 
 async function verifyFundMe(fundMeAddr, args) {
